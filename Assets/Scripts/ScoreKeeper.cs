5d4a4e933885192c5f6d815658a6c3767107e8ee
bhaddock1 <bhaddock25@sandomenico.org>
2023-09-22 12:03:31 -0700

    scorekeeper in prog

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -1,8 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**************************************
+ * attached to player: increases point count on player collision with each platform for the first time.
+ * 
+ * Bryce Haddock 1.0 September 22, 2023
+ * ************************************/
 public class ScoreKeeper : MonoBehaviour
 {
-    
+    [SerializeField] private TextMeshProUGUI scoreboardText;
+    private float score;
+
+    public static ScoreKeeper instance;
+
+    private void Awake()
+    {
+        
+    }
+
+    private void AddToScore(Collision collision)
+    {
+
+    }
 }
+
+    
