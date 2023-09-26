6ded6f47c1e6558f292944eed40d032bde7e779f
bhaddock1 <112724733+bhaddock1@users.noreply.github.com>
2023-09-25 23:50:36 -0700

    comments

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/**************************************************
+* attached to player: moves player in direction of player input W/A/S/D 
+* allows player to jump on jump input (spacebar)
+* launches player upon collision with launchpad tagged game object
+*
+*   Bryce Haddock 1.0 September 25, 2023
+**************************************************/
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
@@ -32,6 +38,7 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody rb;
 
+    // Initializes ready to jump and player rigidbody on start
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -40,40 +47,44 @@ public class PlayerMovement : MonoBehaviour
         readyToJump = true;
     }
 
+    //Updates method every frame 
     private void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); //Searches for ground on frame update if ground is detected player = grounded
 
         MyInput();
         SpeedControl();
 
-        if(grounded)
+        if(grounded)  //eliminates drag when not touching ground
             rb.drag = groundDrag;
         else   
             rb.drag = 0;
-
+            
     }
 
+    //countinuesly checks the move player field 
     private void FixedUpdate()
     {
         MovePlayer();
-    }
+    } 
 
+    // executes method on specified player input
     private void MyInput()
     {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
+        horizontalInput = Input.GetAxisRaw("Horizontal"); // moves player horizontal on horizantal input A/D
+        verticalInput = Input.GetAxisRaw("Vertical");   // moves player vertical on vertical input W/S
 
-        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        if(Input.GetKey(jumpKey) && readyToJump && grounded) //if the player is ready to jump and grounded while jumpkey(spacebar) is pressed the following is executed
         {
-            readyToJump = false;
+            readyToJump = false;    //player is no longer ready to jump
 
-            Jump();
+            Jump(); // jump action is performed
 
-            Invoke(nameof(ResetJump), jumpCooldown);
+            Invoke(nameof(ResetJump), jumpCooldown);    //jump cooldown begins 
         }
     }
 
+    // when the player collides with the launch pad tagged object they will perform the launch action
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("LaunchPad"))
@@ -82,7 +93,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
     }
-
+    
+    // moves the player in the relative direction of player input
+    // if the player is grounded they move at normal move speed
+    //if the player is not grounded they move at normal speed times air multiplier
     private void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
@@ -93,6 +107,7 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);       
     }
 
+    //limits player movement speed to player movement speed 
     private void SpeedControl()
     {
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
@@ -104,6 +119,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    // when the jump action is performed the player will launch into the air at jump speed
     private void Jump()
     {
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
@@ -111,6 +127,7 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
+    // when the launch action is invoked the player will launch into the air at launch speed
     private void Launch()
     {
         rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
@@ -118,6 +135,7 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(transform.up * launchForce, ForceMode.Impulse);
     }
 
+    // resets ready to jump 
     private void ResetJump()
     {
         readyToJump = true;
